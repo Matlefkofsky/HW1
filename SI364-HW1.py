05a571ff4fb812e255db2ec5c3391f96e2161c96
Mathew Lefkofsky <MatLefkofsky@Mathews-MacBook-Pro.local>
2017-09-17 16:21:01 -0400

    finished

--- SI364-HW1.py
+++ SI364-HW1.py
@@ -18,17 +18,27 @@ from flask import Flask
 app = Flask(__name__)
 app.debug = True
 
-@app.route('/')
+@app.route('/class')
 def hello_to_you():
-    return 'Hello!'
-
-
-if __name__ == '__main__':
-    app.run()
+	return 'Welcome to SI 364'
 
 
 ## [PROBLEM 2] - 250 points
 ## Edit the code chunk above again so that if you go to the URL 'http://localhost:5000/movie/<name-of-movie-here-one-word>' you see a big dictionary of data on the page. For example, if you go to the URL 'http://localhost:5000/movie/ratatouille', you should seesomething like the data shown in the included file sample_ratatouille_data.txt, which contains data about the animated movie Ratatouille. However, if you go to the url http://localhost:5000/movie/titanic, you should get different data, and if you go to the url 'http://localhost:5000/movie/dsagdsgskfsl' for example, you should see data on the page that looks like this:
+
+
+import requests
+import json
+
+@app.route('/movie/<name_of_movie>')
+def movie(name_of_movie):
+	parameters = {'term':name_of_movie,"media":"movie"}
+	url = requests.get('https://itunes.apple.com/search?',params=parameters)
+	return url.text
+
+if __name__ == '__main__':
+    app.run()
+
 
 # {
 #  "resultCount":0,
